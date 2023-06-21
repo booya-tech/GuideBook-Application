3813d0c13b8a17750045f9fd3f43171b3308953f
booya-tech <booya.pannachai@gmail.com>
2023-06-21 09:43:43 +0700

    First Commit

---
+++ GuideBook/GuideBook/GuideBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuideBookApp.swift
+//  GuideBook
+//
+//  Created by Panachai Sulsaksakul on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct GuideBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
